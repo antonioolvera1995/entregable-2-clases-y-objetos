306ba9e6efbbb1d3718e00945961b3e9e83107c9
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2020-10-08 18:04:54 +0200

    V3

--- js/index.js
+++ js/index.js
@@ -6,16 +6,42 @@
 // Pide 3 libros y guárdalos en un array ---ok
 // Los libros se introducirán al arrancar el programa pidiendo los datos con prompt.  -----mas tarde
 
-// Validar que los campos no se introduzcan vacíos -----mas tarde
-// Validar que el año sea un número y que tenga 4 dígitos -----mas tarde
-// Validar que el género sea: aventuras, terror o fantasía  -----mas tarde
+// Validar que los campos no se introduzcan vacíos -----ok
+// Validar que el año sea un número y que tenga 4 dígitos -----ok
+// Validar que el género sea: aventuras, terror o fantasía  -----ok
 
 // Crea una función que muestre todos los libros ---ok
 // Crea una función que muestre los autores ordenados alfabéticamente  ----ok
 // Crea una función que pida un género y muestre la información de los libros que pertenezcan a ese género usando un el método que devuelve la información ---ok
 
-let arrayBooks;
 
+
+
+// --------------------------------------------------------------------------------------------
+
+let title1 = document.getElementById('titulo-1');
+let author1 = document.getElementById('author-1');
+let year1 = document.getElementById('year-1');
+let genre1 = document.getElementById('genre-1');
+
+
+let title2 = document.getElementById('titulo-2');
+let author2 = document.getElementById('author-2');
+let year2 = document.getElementById('year-2');
+let genre2 = document.getElementById('genre-2');
+
+
+let title3 = document.getElementById('titulo-3');
+let author3 = document.getElementById('author-3');
+let year3 = document.getElementById('year-3');
+let genre3 = document.getElementById('genre-3');
+
+
+
+
+// --------------------------------------------------------------------------------------------
+
+let arrayBooks;
 class Book {
     constructor(title, author, year, genre) {
 
@@ -45,10 +71,12 @@ class Books {
 
     allBooks() {
 
+        let all = [];
         this.books.forEach(elem => {
+            all.push(elem);
             console.log(elem);
         });
-
+        return all;
     }
 
 
@@ -78,36 +106,26 @@ class Books {
     genreInfo(gen) {
         let fullInfo = '';
         let newBooks = [];
-        let block = false;
+
+
+
+
 
         this.books.forEach(elem => {
-            if (elem.genre === 'Fantasía' || elem.genre === 'Terror' || elem.genre === 'Aventuras') {
-                block = false;
-            } else {
-                block = true;
+            if (elem.genre === gen) {
+                newBooks.push(elem);
             }
         });
 
-        // newBooks.push(elem);
-
-        if (!block) {
-
-
-            this.books.forEach(elem => {
-                if (elem.genre === gen) {
-                    newBooks.push(elem);
-                }
-            });
-
-            for (let i = 0; i < newBooks.length; i++) {
-                if (i >= newBooks.length - 1) {
-                    fullInfo += `Libro ${i+1}: ${newBooks[i].information()}.`
-                } else {
-                    fullInfo += `\nLibro ${i+1}: ${newBooks[i].information()}.`
-                }
+        for (let i = 0; i < newBooks.length; i++) {
+            if (i >= newBooks.length - 1) {
+                fullInfo += `Libro ${i+1}: ${newBooks[i].information()}.`
+            } else {
+                fullInfo += `\nLibro ${i+1}: ${newBooks[i].information()}.`
             }
-
         }
+
+
 
 
 
@@ -117,30 +135,132 @@ class Books {
 }
 
 
-// -------------------------------------------------
-//  let book1 = new Book('El señor de los anillos', ' J. R. R. Tolkien', 1954, 'Fantasía');
-//  let book2 = new Book('Doctor sueño', 'Stephen King', 2013, 'Horror');
-//  let book3 = new Book('Orgugo y Prejuicio', 'Jane Austen', 1813, 'Romántico');
 
-
-
-
-
-
-
-document.querySelector('html').addEventListener("click", function () {
-
+function loadBooks() {
     let book1;
     let book2;
     let book3;
 
-    book1 = new Book('El señor de los anillos', ' J. R. R. Tolkien', 1954, 'Fantasía');
-    book2 = new Book('Doctor sueño', 'Stephen King', 2013, 'Terror');
-    book3 = new Book('Orgugo y Prejuicio', 'Jane Austen', 1813, 'Aventuras');
-
+    book1 = new Book(title1.value, author1.value, year1.value, genre1.value);
+    book2 = new Book(title2.value, author2.value, year2.value, genre2.value);
+    book3 = new Book(title3.value, author3.value, year3.value, genre3.value);
     arrayBooks = [book1, book2, book3];
 
-    let prueba = new Books(arrayBooks);
-    console.log(prueba.genreInfo('Terror'));
+}
+
+
+
+
+
+document.querySelector('#bt-1').addEventListener("click", function () {
+
+    let block = false;
+
+    let all = [title1.value, author1.value, year1.value, genre1.value,
+        title2.value, author2.value, year2.value, genre2.value,
+        title3.value, author3.value, year3.value, genre3.value
+    ];
+
+    let numb = [year1.value, year2.value, year3.value];
+
+    numb.forEach(element => {
+        if (element.length < 4 || element.length > 4) {
+            console.log('La fecha debe de de tener una longitud de 4 números');
+            block = true;
+        } else {
+            block = false;
+        }
+    });
+
+    all.forEach(element => {
+        if (element.length < 1) {
+            console.log('Debes de completar todos los campos, no debes dejar ninguno vacio');
+            block = true;
+        } else {
+            block = false;
+        }
+    });
+
+    if (!block) {
+        loadBooks();
+    }
+
+
 
 });
+
+
+document.querySelector('#bt-2').addEventListener("click", function () {
+
+    try {
+        if (!arrayBooks.length === false) {
+            let bookk = new Books(arrayBooks);
+            console.log(bookk.allBooks());
+
+        } else {
+            console.log('Primero debes de cargar los libros');
+        }
+    } catch (error) {
+        console.log('Primero debes de cargar los libros');
+    }
+
+
+});
+
+document.querySelector('#bt-3').addEventListener("click", function () {
+
+    try {
+        if (!arrayBooks.length === false) {
+            let bookk = new Books(arrayBooks);
+            console.log(bookk.sortAuthors());
+
+        } else {
+            console.log('Primero debes de cargar los libros');
+        }
+    } catch (error) {
+        console.log('Primero debes de cargar los libros');
+    }
+
+});
+
+document.querySelector('#bt-4').addEventListener("click", function () {
+
+    try {
+        if (!arrayBooks.length === false) {
+            let bookk = new Books(arrayBooks);
+            let gen = prompt('Escribe un genero');
+            console.log(bookk.genreInfo(gen));
+
+        } else {
+            console.log('Primero debes de cargar los libros');
+        }
+    } catch (error) {
+        console.log('Primero debes de cargar los libros');
+    }
+
+
+});
+
+function autocomplete() {
+
+
+    title1.value = 'El señor de los anillos';
+    author1.value = ' J. R. R. Tolkien';
+    year1.value = 1954;
+    genre1.value = 'Fantasía';
+
+
+    title2.value = 'Doctor sueño';
+    author2.value = 'Stephen King';
+    year2.value = 2013;
+    genre2.value = 'Terror';
+
+
+    title3.value = 'Orgugo y Prejuicio';
+    author3.value = 'Jane Austen';
+    year3.value = 1813;
+    genre3.value = 'Aventuras';
+
+}
+
+autocomplete();
